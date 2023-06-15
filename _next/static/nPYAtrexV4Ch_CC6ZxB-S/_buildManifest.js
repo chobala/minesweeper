66ad5460a7b037a1a6c4ca697a2c1ee8e419156e
chobala <chobala@users.noreply.github.com>
2023-06-15 16:42:04 +0000

    deploy: 9aebdb177ba6f0d6661bbeaf4d4ccc1dd5fa741e

---
+++ _next/static/nPYAtrexV4Ch_CC6ZxB-S/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5d20ef85ffef5b9c.css","static/chunks/pages/index-69a2ea81f1c33ee0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
